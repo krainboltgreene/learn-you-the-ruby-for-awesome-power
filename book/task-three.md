484230f6ddff4aa73057488b87c4c51c6f297ba8
Kurtis Rainbolt-Greene <kurtisrainboltgreene@gmail.com>
2010-10-23 04:39:55 -0700

    Worked on Task 3.

--- book/task-three.md
+++ book/task-three.md
@@ -3,12 +3,16 @@
 
 
 ### Summary
+Math is an important part of Programming, but don't worry most of the math you'll be doing is very basic. If you ever have to do anything that requires a significant amount of mathematic skill then you'll probably know how it works in the first place.
+
+Like any good programming language, Ruby understands basic math very well.
 
 ![Awesome Placeholder Image](http://dummyimage.com/300/00/44.png&text=Awesome%20Placeholder "So awesome.")
 
 
 ### Source
     <script src=""></script>
+    
     
 
 
--- tasks/lyar3.rb
+++ tasks/lyar3.rb
@@ -3,4 +3,17 @@ author = "Kurtis Rainbolt-Greene"
 # created: 2010.09.13-17:19:18 
 # encoding: utf-8
 
+puts "Preparing For The Market And The Wasteland"
+puts "=========================================="
+puts "The boy looked over his belongings before entering the market."
 
+puts "He had found #{4 + 9} shotgun slugs and #{3 - 2} MRE."
+puts "The lucky dog could smell the #{10 * 3 - 2 - 10} packets of salt."
+
+puts "The boy wondered how much ammo he could get from the merchant!"
+puts 4 + 2 * ( 100 % 2 ) / 4 + ( ( 4 + 2 ) * 4 )
+
+puts "Twenty eight whole bullets! The boy needs it to last ten days, at 2 per day."
+puts "The boy and his lucky dog has enough, right? #{28 / 2 >= 10}"
+puts "Does the boy have enough for twenty days? #{28 / 2 > 20}"
+puts "What about less than 15 days? #{28 / 2 < 15}"
